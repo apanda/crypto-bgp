aeed0e332d3cdb628eab5a11462f0b27e0f99482
Aurojit Panda <apanda@cs.berkeley.edu>
2012-09-28 16:45:30 -0700

    Remove unnecessary files in preparation for some automation

--- scripts/IDR1/peer01/runPeer01.bat
+++
@@ -1,2 +0,0 @@
-java -cp "../../sepia.jar;../../IDR.jar" -Djavax.net.ssl.trustStore=peer01KeyStore.jks  MainCmd -p 0 -c config.peer01.properties
-pause
--- scripts/IDR2/privacypeer03/runPrivacyPeer03.bat
+++
@@ -1,2 +0,0 @@
-java -cp "../../sepia.jar;../../pptks.jar" -Djavax.net.ssl.trustStore=privacypeer03KeyStore.jks  MainCmd -p 1 -c config.privacypeer03.properties
-pause
