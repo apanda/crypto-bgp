f0feb22ca05212769f3cabdd6264409b96655489
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2012-11-05 14:51:46 -0800

    Purge.

--- scripts/IDR3/privacypeer01/runPrivacyPeer01.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-java -cp "../../lib/sepia.jar:../../lib/mpc.jar" -Djavax.net.ssl.trustStore=privacypeer01KeyStore.jks  MainCmd -p 1 -c config.privacypeer01.properties
-#java -cp "../../sepia.jar:../../mpc.jar" -Djavax.net.ssl.trustStore=privacypeer01KeyStore.jks  MainCmd -v -p 1 -c config.privacypeer01.properties
-#java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=10044 -cp "../../sepia.jar:../../mpc.jar" -Djavax.net.ssl.trustStore=privacypeer01KeyStore.jks  MainCmd -v -p 1 -c config.privacypeer01.properties
--- scripts/startDaemons.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting daemons..."
-java -jar lib/daemon.jar -Listen 50100 -Connections $1 &> dlog/d1.log &
-java -jar lib/daemon.jar -Listen 50200 -Connections $1 &> dlog/d2.log &
-java -jar lib/daemon.jar -Listen 50300 -Connections $1 &> dlog/d3.log &
