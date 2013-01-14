4f6e668427112180a0e33ac14543695e77778418
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-13 19:25:14 -0800

    Bug fixes.

--- net/rpc_client.cpp
+++ net/rpc_client.cpp
@@ -142,8 +142,7 @@ void RPCClient::publish(string key,  int64_t value, vertex_t vertex) {
 
 
   LOG4CXX_TRACE(logger_, "Sending value: " << key << ": " << value << " (" << vertex << ")");
-
-  LOG4CXX_TRACE(logger_, "Sending to: " << socket_.remote_endpoint().address().to_string());
+  //LOG4CXX_TRACE(logger_, "Sending to: " << socket_.remote_endpoint().address().to_string());
 
   boost::asio::async_write(socket_,
       boost::asio::buffer(data, length_),
