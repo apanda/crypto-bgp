3efe80fa9276537af5e9e3e9aacb3969dd67a7e3
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-17 17:04:30 -0800

    Additional logging.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -213,7 +213,7 @@ void BGPProcess::process_neighbors_mpc(
       std::insert_iterator< std::vector<vertex_t> >( intersection, intersection.begin() ) );
 
   LOG4CXX_INFO(comp_peer_->logger_, "Intersection size for vertex "
-      << affected.id_ << ": " << intersection.size());
+      << affected.id_ << ": " << intersection.size() << ": " << neighs.size());
 
   for0(affected_vertex, new_changed_set_ptr, counts_ptr, intersection_ptr);
 
