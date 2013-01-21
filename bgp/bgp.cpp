13be79a953bb3caa09b2bb4f2f8788187eb18729
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-20 20:01:12 -0800

    Bug fixes.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -299,18 +299,22 @@ void BGPProcess::compute_partial0(
 
       }
 
-      intersection.assign(local_set.begin(), local_set.end());
-      std::sort(intersection.begin(), intersection.end());
+      shared_ptr< vector<vertex_t> > intersection_ptr2(new vector<vertex_t>());
+      //intersection.assign(local_set.begin(), local_set.end());
+      std::sort(intersection_ptr2->begin(), intersection_ptr2->end());
+
+      LOG4CXX_DEBUG(comp_peer_->logger_, "intersection " << intersection.size());
 
       partial_count = 0;
       partial_batch_count = 1;
 
 
-      auto new_pair = std::make_pair(intersection.begin(), intersection.end());
+      auto new_pair = std::make_pair(intersection_ptr2.begin(), intersection_ptr2.end());
+      m_.unlock();
 
       compute_partial0(
           affected_vertex, largest_vertex_ptr, new_changed_set_ptr, local_set_ptr,
-          global_counter_ptr, local_counter_ptr, intersection_ptr, new_pair);
+          global_counter_ptr, local_counter_ptr, intersection_ptr2, new_pair);
 
       return;
     }
