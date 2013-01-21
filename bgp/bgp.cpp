316e81d048820a86d31473fece4d8a4f9bb76dc2
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-20 21:32:21 -0800

    Bug fixes.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -287,6 +287,8 @@ void BGPProcess::compute_partial0(
 
     if (partial_batch_count == 1) {
       LOG4CXX_INFO(comp_peer_->logger_, "second iteration "<< partial_count << " " << partial_batch_count);
+      LOG4CXX_INFO(comp_peer_->logger_, "second iteration x "<< local_counter_ptr->first << " " << local_counter_ptr->second);
+
 
       if (partial_count == partial_batch_count) {
         count++;
