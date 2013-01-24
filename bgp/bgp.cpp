fbd293aaf503f5e7e3375366b273235f42273796
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-24 15:24:40 -0800

    Fix.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -669,6 +669,8 @@ void BGPProcess::load_state(string path, graph_t& graph) {
 
     if(tokens.size() != 2) throw std::runtime_error( lexical_cast<string>(tokens.size()) );
 
+    std::cout << src << std::endl;
+
     vertex_t src = lexical_cast<size_t>(tokens[0]);
     vertex_t dst = lexical_cast<size_t>(tokens[1]);
 
