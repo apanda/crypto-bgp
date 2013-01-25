182d40695cdbf0adbf20e94e78e38bd12945398e
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-24 18:23:42 -0800

    Iter crap.

--- main.cpp
+++ main.cpp
@@ -115,7 +115,6 @@ void run_mpc() {
     cp->bgp_->master_ = master;
     init.id_ = cp->id_;
 
-    LOG4CXX_INFO(mainLogger, "master->init(init))");
     master->init(init);
   }
 
@@ -128,15 +127,12 @@ void run_mpc() {
   vector<vertex_t> nodes;
   for(auto& cp: comp_peer_seq) {
     if (COMP_PEER_IDS.find(cp->id_) == COMP_PEER_IDS.end()) continue;
-    LOG4CXX_INFO(mainLogger, "master->sync(nodes)");
     master->sync(nodes);
   }
 
   master->barrier_->wait();
 
   LOG4CXX_INFO(mainLogger, "Master has raised the barrier.");
-
-  //sleep(10);
 
   master->barrier_ = new boost::barrier(COMP_PEER_IDS.size() + 1);
 
@@ -170,6 +166,7 @@ int main(int argc, char *argv[]) {
       ("start", po::value<size_t>(), "staring vertex")
       ("end", po::value<size_t>(), "ending vertex")
       ("limit", po::value<size_t>(), "per vertex limit")
+      ("sever", po::value<size_t>(), "which link to sever")
       ("id", po::value<vector<int>>()->multitoken(), "computational peer id")
   ;
 
@@ -199,6 +196,11 @@ int main(int argc, char *argv[]) {
 
   if (vm.count("end")) {
     VERTEX_END = vm["end"].as<size_t>();
+  }
+
+  if (vm.count("sever")) {
+    SEVER_FLAG = true;
+    SEVER_EDGE = vm["sever"].as<size_t>();
   }
 
   if (vm.count("limit")) {
