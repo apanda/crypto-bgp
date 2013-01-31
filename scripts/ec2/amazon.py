c0e879d32a6b70ab91a73c785be08e33dfde5917
Vjekoslav Brajkovic <vjeko@tanagra.CS.Berkeley.EDU>
2013-01-30 20:21:20 -0800

    Export policy.

--- scripts/ec2/amazon.py
+++ scripts/ec2/amazon.py
@@ -218,7 +218,7 @@ def delegate(instances, graphSize, master):
   MASTER = master
   THREADS = 8
   TASKS = 1
-  LIMIT = 20
+  LIMIT = 200
   WHOAMI = '`/sbin/ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | \
   awk \'{ print $1}\' `'
   
