8b54d4b0f8ac51a6eaf3cfe68b29f6ab29dcd8bb
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-05-07 18:05:59 -0700

    Bug fix.

--- peer/comp_peer_template.hpp
+++ peer/comp_peer_template.hpp
@@ -242,7 +242,10 @@ void CompPeer<Num>::multiply(
 
   const string key = recombination_key + "_" + lexical_cast<string>(id_);
   const int64_t result = mod(vlm[first] * vlm[second], PRIME);
-  LOG4CXX_TRACE( logger_, "CompPeer<Num>::multiply " << recombination_key);
+
+  LOG4CXX_TRACE( logger_, "CompPeer<Num>::multiply " << result << " = " << vlm[first] << " * " << vlm[second]);
+
+  //LOG4CXX_TRACE( logger_, "CompPeer<Num>::multiply " << recombination_key);
 
 
   vertex.sig_recombine[recombination_key] = shared_ptr< boost::function<void ()> >(
@@ -276,7 +279,7 @@ void CompPeer<Num>::recombine(string recombination_key, vertex_t l) {
       //throw std::runtime_error(error);
     }
 
-    const int64_t& value = vlm[key];
+    const int64_t& value = mod(vlm[key], PRIME);
 
     gsl_vector_set(ds, i,  value);
     debug_stream <<  " " << value;
