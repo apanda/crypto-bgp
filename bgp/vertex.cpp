a35be820eb57a6099dcea8911ee0f558b68f1ad7
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-02 20:44:39 -0700

    Bug fix.

--- bgp/vertex.cpp
+++ bgp/vertex.cpp
@@ -12,7 +12,7 @@ Vertex::Vertex() :
 
 int Vertex::get_export(vertex_t to_vertex) {
 
-  std::cout << "Next hop " << next_hop_;
+  std::cout << "Next hop " << next_hop_ << std::endl;
 
   BOOST_ASSERT( relationship_.find(next_hop_) != relationship_.end() );
   BOOST_ASSERT( relationship_.find(to_vertex) != relationship_.end() );
