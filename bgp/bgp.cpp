555f8cfe631dbbd9ce62191c963a269964cd6252
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-02 22:44:29 -0700

    Bug fix.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -238,9 +238,14 @@ void BGPProcess::process_neighbors_mpc(const vertex_t affected_vertex,
   }
 
 
+  compute_local.erase(
+      std::remove(compute_local.begin(), compute_local.end(), 0),
+      compute_local.end()
+      );
+
   std::sort(compute_local.begin(), compute_local.end(),
       boost::bind(&pref_pair_t::second, _1)
-          > boost::bind(&pref_pair_t::second, _2));
+          < boost::bind(&pref_pair_t::second, _2));
 
   auto pair = compute_local.front();
   const vertex_t offered_vertex = pair.first;
@@ -254,7 +259,7 @@ void BGPProcess::process_neighbors_mpc(const vertex_t affected_vertex,
 
   std::sort(prefs.begin(), prefs.end(),
       boost::bind(&pref_pair_t::second, _1)
-          > boost::bind(&pref_pair_t::second, _2));
+          < boost::bind(&pref_pair_t::second, _2));
 
 
   vlm["result"] = 0;
