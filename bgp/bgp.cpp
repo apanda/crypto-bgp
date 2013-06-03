0ed1454a1a0e2782340f95592b11eb54ec246373
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-02 22:51:22 -0700

    Bug fix.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -253,9 +253,9 @@ void BGPProcess::process_neighbors_mpc(const vertex_t affected_vertex,
   } else {
     auto pair = compute_local.front();
     offered_vertex = pair.first;
-    Vertex& offered = graph_[offered_vertex];
   }
 
+  Vertex& offered = graph_[offered_vertex];
 
   if (offered_vertex != 0 && offered_vertex != affected.next_hop_) {
     affected.new_next_hop_ = offered_vertex;
