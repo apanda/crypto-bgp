ab6dd298a7bd239f4363d7c11070cf801e230d49
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-04 16:07:19 -0700

    Bug fix.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -218,11 +218,6 @@ void BGPProcess::process_neighbors_mpc(const vertex_t affected_vertex,
     auto pref = affected.preference_[neigh];
     auto pref_export = pref * offered.get_export(affected_vertex) ;
 
-    if (!offered.loop_free(graph_, affected_vertex)) {
-      pref = 0;
-      pref_export = 0;
-    }
-
     prefs.push_back( pref_pair_t(neigh, pref) );
     compute_local.push_back( pref_pair_t(neigh, pref_export) );
   }
