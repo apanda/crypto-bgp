cd4a13ec0cf47d5fc075dc0cfde4bab0862b3904
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-09 20:50:41 -0700

    New.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -137,7 +137,7 @@ void BGPProcess::next_iteration_continue(const vertex_t dst_vertex,
   int counter = 0;
   while (true) {
     if(work_queue_.empty()) break;
-    if(counter > 300) break;
+    if(counter > 1024) break;
 
     boost::function<void()> f;
     bool popped = work_queue_.try_pop(f);
