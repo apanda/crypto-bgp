fc0f5bea84b160e6b8952bd6e78eb14f02ae9e06
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-12 18:53:26 -0700

    New.

--- net/rpc_client.cpp
+++ net/rpc_client.cpp
@@ -187,7 +187,10 @@ void RPCClient::write_loop() {
   }
 
   size_t size = data_vec.size();
-  if (size) {
+  if (size > 0) {
+
+    std::cout << "size > 0" << std::endl;
+
     char* new_data = new char[length_ * size];
     for(auto i = 0; i < size; i++) {
       memcpy(new_data + length_ * i, data_vec[i], length_);
