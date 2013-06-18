ea853cf9f9854aad4a71f2d503a0f04cd168a024
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-17 18:55:02 -0700

    New.

--- net/rpc_client.cpp
+++ net/rpc_client.cpp
@@ -133,7 +133,7 @@ void RPCClient::publish(string key,  int64_t value, vertex_t vertex) {
   uint32_t& size =  *((uint32_t*) (data + sizeof(uint32_t)));
 
   command = CMD_TYPE::MSG;
-  size = length_;
+  size = 276;
 
   BOOST_ASSERT(key.length() < (msg_ - sizeof(vertex_t) - sizeof(int64_t)));
 
