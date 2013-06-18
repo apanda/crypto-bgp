3eab6f0c4e9f7d9e9a0c6bd43e3b5f9b93284d0c
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-17 20:21:16 -0700

    New.

--- common.cpp
+++ common.cpp
@@ -16,6 +16,25 @@ int mod( int64_t x, int m) {
     return (x%m + m)%m;
 }
 
+void hexdump(void *ptr, int buflen) {
+  unsigned char *buf = (unsigned char*)ptr;
+  int i, j;
+  for (i=0; i<buflen; i+=16) {
+    printf("%06x: ", i);
+    for (j=0; j<16; j++)
+      if (i+j < buflen)
+        printf("%02x ", buf[i+j]);
+      else
+        printf("   ");
+    printf(" ");
+    for (j=0; j<16; j++)
+      if (i+j < buflen)
+        printf("%c", isprint(buf[i+j]) ? buf[i+j] : '.');
+    printf("\n");
+  }
+}
+
+
 bool is_number(const std::string& s)
 {
     return !s.empty() && std::find_if(s.begin(),
