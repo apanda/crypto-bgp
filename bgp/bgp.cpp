3b2722f3c21537b97b010c10ccbdb896ae491fda
Vjekoslav Brajkovic <balkan@cs.washington.edu>
2013-06-18 13:12:11 -0700

    New.

--- bgp/bgp.cpp
+++ bgp/bgp.cpp
@@ -502,6 +502,8 @@ void BGPProcess::for_add(const vertex_t affected_vertex,
 
   local.first++;
 
+  LOG4CXX_INFO(comp_peer_->logger_, "  local.first == " <<   local.first);
+
   if (local.first == local.second) {
 
     affected.couter_map_.clear();
